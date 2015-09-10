f573e1cce27cbd36b407645920bc343974581f4e
Volodymyr Sapsai <vsapsai@gmail.com>
2015-09-09 22:40:02 -0700

    Add test case to catch when naive fix is doing the wrong thing.

---
+++ tests/cxx/issue166_naive_fix-d1.h
@@ -0,0 +1,12 @@
+//===--- issue166_naive_fix-d1.h - test input file for iwyu ---------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tests/cxx/direct.h"
+
+typedef IndirectClass IndirectClassTypedef;
