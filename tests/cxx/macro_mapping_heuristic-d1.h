4a75e33c83c48d25a4b2c742cccf937f8ce6e230
Volodymyr Sapsai <vsapsai@gmail.com>
2016-04-10 12:40:36 -0700

    Better test coverage.

--- tests/cxx/macro_mapping_heuristic-d1.h
+++
@@ -1,2 +0,0 @@
-#define DIRECT_INCLUDE_GUARD
-#include "tests/cxx/macro_mapping_heuristic-i1.h"
--- tests/cxx/macro_mapping_heuristic-i1.h
+++
@@ -1,5 +0,0 @@
-#ifndef DIRECT_INCLUDE_GUARD
-  #error Do not include directly
-#else
-  class MacroMappingHeuristicI1 {};
-#endif
